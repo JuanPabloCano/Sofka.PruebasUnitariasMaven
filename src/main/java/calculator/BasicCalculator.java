66375492fd05c1c4ffcfe9a5607e983c82bd23eb
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-12-17 20:48:06 -0500

    Se generan los Tests de cada una de las operaciones

--- src/main/java/calculator/BasicCalculator.java
+++ src/main/java/calculator/BasicCalculator.java
@@ -1,4 +1,32 @@
 package calculator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long subtraction(Long number1, Long number2) {
+        logger.info( "Subtracting {} + {}", number1, number2 );
+        return number1 - number2;
+    }
+
+    public Long multiplication(Long number1, Long number2) {
+        logger.info( "Multiplying {} + {}", number1, number2 );
+        return number1 * number2;
+    }
+
+    public Long division(Long number1, Long number2) throws IllegalArgumentException {
+        if (number2 == 0){
+            throw new IllegalArgumentException("Error, no se puede dividir por cero");
+        }
+        logger.info( "Dividing {} + {}", number1, number2 );
+        return number1 / number2;
+    }
 }
